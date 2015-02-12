3d1fcaa1b8c057baae54a46c5a876b63d5be1fef
Christopher Wood <christopherwood07@gmail.com>
2015-02-11 22:14:51 -0800

    Updating Util

--- src/src/util/Util.java
+++ src/src/util/Util.java
@@ -1,16 +1,15 @@
 package util;
 
-import dispatch.Clock;
-
 public class Util
 {
+	
 	public static void disp(String s)
 	{
-		System.out.println("[" + Clock.time + "] " + s);
+//		System.out.println("[" + Clock.time + "] " + s);
 	}
 	
 	public static void error(String s)
 	{
-		System.err.println("[" + Clock.time + "]> " + s);
+//		System.err.println("[" + Clock.time + "]> " + s);
 	}
 }
